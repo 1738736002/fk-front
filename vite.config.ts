c019fdfc0ea567bf87250e8177beb21514c62f15
xuwenwen <wenwen.xu@nicezhuanye.com>
2024-07-28 19:08:42 +0800

    icon

--- vite.config.ts
+++ vite.config.ts
@@ -47,7 +47,19 @@ export default defineConfig({
         name: 'My Awesome App',
         short_name: 'MyApp',
         description: 'My Awesome App description',
-        theme_color: '#ffffff'
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       },
       workbox: {
         cleanupOutdatedCaches: false
